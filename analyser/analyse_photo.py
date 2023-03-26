13f8a69db4f30bdb86a63d101062f251b0a61875
Tanya Ignatenko <tanigna.work@gmail.com>
2023-03-26 19:43:22 +0700

    Fix

--- analyser/analyse_photo.py
+++ analyser/analyse_photo.py
@@ -18,20 +18,19 @@ def f(x):
     return x * 1./255
 
 def is_fake(img):
-#     model = get_model(model_name)
-#
-#     img = apply_ela(img)
-#     img = cv2.resize(img, (img_width, img_height))
-#
-#     unused, img_data = cv2.imencode('.bmp', img)
-#     img = Image.open(io.BytesIO(img_data))
-#
-#     prediction_data = datagen.standardize(image.img_to_array(img))
-#     prediction_data = np.expand_dims(prediction_data, axis=0)
-#
-#     predictions = model.predict(prediction_data, batch_size=20, verbose=1)
-#
-#     result = class_map[0 if predictions[0][0] < 0.98 else 1]
-#
-#     return result == 'modified'
-      return True
+    model = get_model(model_name)
+
+    img = apply_ela(img)
+    img = cv2.resize(img, (img_width, img_height))
+
+    unused, img_data = cv2.imencode('.bmp', img)
+    img = Image.open(io.BytesIO(img_data))
+
+    prediction_data = datagen.standardize(image.img_to_array(img))
+    prediction_data = np.expand_dims(prediction_data, axis=0)
+
+    predictions = model.predict(prediction_data, batch_size=20, verbose=1)
+
+    result = class_map[0 if predictions[0][0] < 0.98 else 1]
+
+    return result == 'modified'
