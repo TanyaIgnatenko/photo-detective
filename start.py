4ffe47cfa6c51e9df9df2494eb2fa72c8b4aaac6
Tanya Ignatenko <tanigna.work@gmail.com>
2023-03-27 18:29:31 +0700

    Remove unnecessary route

--- start.py
+++ start.py
@@ -9,11 +9,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
 @app.route('/check', methods=['POST'])
 def check():
     if request.method == 'POST':
